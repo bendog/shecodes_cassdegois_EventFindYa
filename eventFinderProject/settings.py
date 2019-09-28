98cb4fd87bc5ab96bb812d461ca845e8056d78e1
Ben Fitzhardinge <ben@fitzhardinge.net>
2019-09-28 20:48:17 +0800

    updated settings for ben's local machine

--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -32,18 +32,20 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "eventFinderApp.apps.EventfinderappConfig",
+    "users.apps.UsersConfig",
+    # django
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "users.apps.UsersConfig",
+    # 3rd party
     "crispy_forms",
     "django_filters",
-    # 'bootstrapforms',
     "rest_framework",
     "rest_framework.authtoken",
+    # 'bootstrapforms',
 ]
 
 REST_FRAMEWORK = {
@@ -86,14 +88,21 @@ WSGI_APPLICATION = "eventFinderProject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "eventfindya",  # you will need to create this db
+#         "USER": "cassandragosby",  # enter your user name here
+#         "PASSWORD": "Shecodes123",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "eventfindya",  # you will need to create this db
-        "USER": "cassandragosby",  # enter your user name here
-        "PASSWORD": "Shecodes123",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
